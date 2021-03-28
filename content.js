c2eaa3f9947339f75159fad61746fe7823c81324
IvanPlease <szewczykjan2@gmail.com>
2021-03-28 14:34:47 +0200

    New function addition

--- content.js
+++ content.js
@@ -2,26 +2,51 @@ chrome.runtime.onMessage.addListener(
     function(request){
         switch(request){
             case "save-project":
+
                 if($("#cpEditorSaveButton").is(":visible")){
                     $("#cpEditorSaveButton").trigger("click");
                 }
+
+                if($("button[data-theme=positive]").is(":visible")){
+                    $("button[data-theme=positive]").trigger("click");
+                    clearInterval(i);
+                }
+
                 var i = setInterval(function(){
+                    console.log("checking for button");
                     if($("#gzEditorSaveButton").is(":visible")){
                         $("#gzEditorSaveButton").trigger("click");
                         clearInterval(i);
                     }
-                }, 200);
+                }, 100);
                 break;
             case "duplicate-checkpoint":
                 $("#cpEditorDuplicateButton").trigger("click");
                 break;
             case "delete-checkpoint":
                 $("#cpEditorDeleteButton").trigger("click");
-                $("button[data-theme=positive]").trigger("click");
+                checkForButton();
                 break;
             case "cancel-edition":
                 $("#cpEditorCancelButton").trigger("click");
                 break;
+            case "new-checkpoint":
+                $("#addCheckPointButton").find("span").first().trigger("click");
+                break;
+            case "new-connection":
+                $("#addEdgeButton").find("span").first().trigger("click");
+                checkForButton();
+                break;
         }
     }
 )
+
+function checkForButton(){
+    var i = setInterval(function(){
+        console.log("checking for button");
+        if($("button[data-theme=positive]").is(":visible")){
+            $("button[data-theme=positive]").trigger("click");
+            clearInterval(i);
+        }
+    }, 100);
+}
